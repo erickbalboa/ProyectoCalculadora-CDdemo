037ac6c1b664b14f8a8be5794f83ea220cc578bf
JJ Ruescas <juanjose.ruescas@gmail.com>
2017-09-14 09:55:31 -0400

    Unit Tests Proyect Added

---
+++ ProyectoCalculadora/Controllers/Calculadora.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoCalculadora.Controllers
+{
+    public class Calculadora
+    {   
+        public int Suma(int a, int b)
+        {
+            return (a + b);
+        }
+
+        public int Division(int a, int b)
+        {
+            if (b == 0)
+                throw new Exception("mesage");
+            return 0;  
+        }
+    }
+}
---
+++ UnitTests-ProyectoCalculadora/UnitTestCalculadora.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProyectoCalculadora.Controllers;
+
+namespace UnitTests_ProyectoCalculadora
+{
+    [TestClass]
+    public class UnitTestCalculadora
+    {
+        [TestMethod]
+        public void TestSuma()
+        {
+            var calculadora = new Calculadora();
+            var result = calculadora.Suma(1, 2);
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "Division entre cero no permitida.")]
+        public void TestDivisionEntreCero()
+        {
+            var calculadora = new Calculadora();
+            var result = calculadora.Division(2, 0);
+        }
+    }
+}
