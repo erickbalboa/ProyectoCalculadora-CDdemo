84958a56e7d52f2221231d6b01ac083101d9b0b5
JJ Ruescas <juanjose.ruescas@gmail.com>
2017-09-14 22:45:14 -0400

    NUnit Added

---
+++ NUnit-ProyectoCalculadora/TestCalculadora.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProyectoCalculadora.Controllers;
+using NUnit.Framework;
+
+namespace NUnit_ProyectoCalculadora
+{
+    [TestFixture]
+    public class TestCalculadora
+    {
+        [Test]
+        public void TestSuma()
+        {
+            var calculadora = new Calculadora();
+            var result = calculadora.Suma(1, 2);
+            Assert.AreEqual(3, result);
+        }
+    }
+}
---
+++ ProyectoCalculadora/Controllers/Calculadora.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoCalculadora.Controllers
+{
+    public class Calculadora
+    {
+        public int Suma(int a, int b)
+        {
+            return 3;
+        }
+    }
+}
